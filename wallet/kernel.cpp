925f56fcb5359ef0f86f5631663fbe7a2bdb8e4d
MorningStar Payments Official <pennsylvaniamma@hotmail.com>
2018-06-02 00:27:22 -0400

    Update kernel.cpp

--- wallet/kernel.cpp
+++ wallet/kernel.cpp
@@ -18,7 +18,7 @@ static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
-static std::map<int, unsigned int> mapStakeModifierCheckpoints =
+static std::map<int, unsigned int> mapStakeModifierCheckpointsTestNet =
     boost::assign::map_list_of
         ( 0,    0x0e00670b ) // genesis
     ;
